40d2a44213b9cab0c1ba9173f660b8d7554af6f3
YanaGolodyuk <31563214+YanaGolodyuk@users.noreply.github.com>
2022-05-04 01:29:04 +0300

    project created

---
+++ EmojiReader/EmojiReader/GradientView.swift
@@ -0,0 +1,33 @@
+//
+//  GradientView.swift
+//  EmojiReader
+//
+//  Created by admin on 04.05.2022.
+//
+
+import Foundation
+import UIKit
+
+class GradientView: UIView {
+    
+    private let gradientLayer = CAGradientLayer()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupGradient()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientLayer.frame = bounds
+    }
+    
+    private func setupGradient() {
+        self.layer.addSublayer(gradientLayer)
+        gradientLayer.colors = [UIColor.red.cgColor, UIColor.yellow.cgColor]
+    }
+}
