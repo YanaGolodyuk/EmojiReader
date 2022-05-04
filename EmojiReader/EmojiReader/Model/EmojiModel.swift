be5461724543824494020517b64b77791315557d
YanaGolodyuk <31563214+YanaGolodyuk@users.noreply.github.com>
2022-05-04 05:38:23 +0300

    Done
    Little TVC project

---
+++ EmojiReader/EmojiReader/Model/EmojiModel.swift
@@ -0,0 +1,15 @@
+//
+//  EmojiModel.swift
+//  EmojiReader
+//
+//  Created by admin on 04.05.2022.
+//
+
+import Foundation
+
+struct Emoji {
+    var emoji: String
+    var name: String
+    var description: String
+    var isFavourite: Bool
+}
